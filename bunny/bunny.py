33648996f14d95b94c7d52efecc7ff511b7e5c53
Charlie <gibsticker@gmail.com>
2015-02-16 17:14:54 -0500

    added argument parsing

--- bunny/bunny.py
+++ bunny/bunny.py
@@ -3,13 +3,13 @@ __author__ = 'Charlie'
 import envoy
 from glob import glob
 
+
 def run_test(program, in_file, out_file):
     with open(in_file, 'r') as f:
-      input_data = f.read()
+        input_data = f.read()
 
     with open(out_file, 'r') as f:
-      output_data = f.read()
-
+        output_data = f.read()
 
     test = envoy.run(program, data=input_data, timeout=30)
     result_data = test.std_out
@@ -20,26 +20,34 @@ def run_test(program, in_file, out_file):
     else:
         return 'failed: expected\n' + output_data + '\nreceived:\n' + result_data
 
+
 def main():
-    import sys
     import argparse
 
-    if not len(sys.argv) == 2:
-        print('Usage: bunny <program to test>')
-        sys.exit()
+    # setup parser and parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('program', help='binary to test')
+    parser.add_argument('-t', "--test-dir", metavar='path', help='Path to test directory (default: tests)',
+                        action='store')
+    parser.add_argument('-i', '--input', metavar='ext', help='File extension for input file (default: .in)')
+    parser.add_argument('-o', '--output', metavar='ext', help='File extension for output file (default: .expect')
+    args = parser.parse_args()
 
-    program = sys.argv[1]
+    # store parsed arguments
+    test_dir = args.test_dir or 'tests'
+    program = args.program
+    in_ext = args.input or '.in'
+    out_ext = args.output or '.expect'
 
-    IN_EXT = '.in'
-    OUT_EXT = '.expect'
-
-    in_files = tuple((glob('tests/*' + IN_EXT)))
-    out_files = tuple((glob('tests/*' + OUT_EXT)))
+    in_files = tuple((glob(test_dir + '/*' + in_ext)))
+    out_files = tuple((glob(test_dir + '/*' + out_ext)))
 
     results = map(lambda x, y: run_test(program, x, y), in_files, out_files)
+
     print("Testing...")
     for result in results:
         print(result)
+
 
 if __name__ == "__main__":
     main()
